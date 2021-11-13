a56a54bbe0d3f847c9df651cb0840e669c3d8cca
ASUSTe-UX434IQ <redhat@Stewart>
2021-11-13 14:33:08 -0700

    More CSS variables

---
+++ code.js
@@ -0,0 +1,12 @@
+/*
+  + Code to create new records.
+  + An edited state.
+  + 
+*/
+
+function script(){alert(1)}
+
+list=document.querySelectorAll(".field.scroll .words")
+lambda=list[0]
+
+lambda.onblur=script
